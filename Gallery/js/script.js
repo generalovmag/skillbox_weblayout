5c9c1f7353f12b138ed177bbf33b5b0d5f81d2f5
Maxim Generalov <generalov.mag@yandex.ru>
2022-09-22 13:00:53 +0300

    up gallery desktop version

--- Gallery/js/script.js
+++ Gallery/js/script.js
@@ -46,6 +46,18 @@ const swiperEvents = new Swiper('.events__slider', {
   },
 });
 
+// Swiper Projects
+
+const swiperProjects = new Swiper('.projects__slider', {
+  slidesPerView: 1,
+  loop: true,
+
+  // навигация
+  navigation: {
+    nextEl: '.projects__slider .swiper-button-next',
+    prevEl: '.projects__slider .swiper-button-prev',
+  },
+});
 
 // Akkordion
 
@@ -54,3 +66,29 @@ $(".accordion").accordion({
   active: 0
   });
   
+// Yandex map
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("yndex-map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.76, 37.60],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 15
+  });
+
+  var myPlacemark = new ymaps.Placemark([55.76, 37.60], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: '/img/1920px/map-icon.png',
+    iconImageSize: [20, 20],
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+};
